fdf454fa8acced5bb19cb385312b46c1339db3f1
Kevin <kevin.tai.mail@gmail.com>
2018-04-09 16:19:53 -0700

    viewcontrollers created

---
+++ FinalProject/ViewControllers/DealViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DealViewController.swift
+//  FinalProject
+//
+//  Created by KevinT on 2018-04-09.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+import UIKit
+
+class DealViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
