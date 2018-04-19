2690a70da00ffad4430ed9854c373712277ad839
Kevin <kevin.tai.mail@gmail.com>
2018-04-19 12:07:06 -0700

    ibactions added

---
+++ FinalProject/ViewControllers/MenuButtonViewController.swift
@@ -0,0 +1,60 @@
+//
+//  MenuButtonViewController.swift
+//  FinalProject
+//
+//  Created by KevinT on 2018-04-19.
+//  Copyright © 2018 KevinT. All rights reserved.
+//
+
+import UIKit
+
+class MenuButtonViewController: UIViewController {
+
+  
+  
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+  @IBAction func randomDealsTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func foodDealsTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func drinkDealsTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func dateDealsTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func allDealsTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func myDealsTapped(_ sender: UIButton) {
+  }
+  
+  @IBAction func clearMyDealsTapped(_ sender: UIButton) {
+  }
+  
+
+  /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
